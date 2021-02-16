81ddd2f5760ea9e7b01630746d217be3a203d8b1
browkyle <77990267+browkyle@users.noreply.github.com>
2021-02-15 22:23:20 -0500

    Finished

--- Bullet/Bullet.gd
+++ Bullet/Bullet.gd
@@ -15,3 +15,7 @@ func _on_Bullet_body_entered(body):
 	Explosions.add_child(explosion)
 	body.queue_free()
 	queue_free()
+
+
+func _on_Area2D_body_entered(body):
+	pass # Replace with function body.
